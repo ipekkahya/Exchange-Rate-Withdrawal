55e518717412aa6f2701cc9021c7607fa62e24ac
ipekkahya <39248066+ipekkahya@users.noreply.github.com>
2021-07-26 21:11:17 +0300

    Create Default.aspx.cs

---
+++ Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace exchangeratewithdrawal
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string today = "http://www.tcmb.gov.tr/kurlar/today.xml";
+
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(today);
+
+            string USD_Alis = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteBuying").InnerXml;
+            string USD_Satis = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteSelling").InnerXml;
+
+            string EURO_Alis = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteBuying").InnerXml;
+            string EURO_Satis = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteSelling").InnerXml;
+
+            if(DropDownList1.SelectedValue == "1")
+            {
+                Label1.Text = USD_Alis.ToString();
+            }
+
+            if (DropDownList1.SelectedValue == "2")
+            {
+                Label1.Text = EURO_Alis.ToString();
+            }
+        }
+
+    }
+}
+
